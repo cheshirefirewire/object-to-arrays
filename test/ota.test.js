dcf1aa16dede23bbb460db533eba1b3e1d404d22
raydecastro <captray108@gmail.com>
2017-02-13 21:36:35 -0800

    feature. added test for get keys and values.

--- test/ota.test.js
+++ test/ota.test.js
@@ -1,6 +1,9 @@
 import {expect} from "chai";
 import Ota from "../source/ota.js";
 
+// create a function that takes an object and create two arrays of it, one for all the keys, one for
+// all the values. Return an array of the two arrays
+
 describe("ota", () => {
     it("shall exist", () => {
         let ota = new Ota();
@@ -8,4 +11,19 @@ describe("ota", () => {
         expect(ota).to.be.defined;
     });
 
+    it("shall have the ability to create two arrays (one for keys, another for values) from original object", () => {
+        let ota = new Ota();
+        let inputObject = {
+            key1: 1,
+            key2: 2,
+            key3: 3,
+            key4: 4
+        };
+        let expectedOutput = [
+            ['key1', 'key2', 'key3', 'key4'],
+            [1, 2, 3, 4]
+        ];
+
+        expect(ota.getKeysAndValues(inputObject)).to.deep.equal(expectedOutput);
+    });
 });

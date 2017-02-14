8cbcbf305fdf31f5796dc2ad6ea7887a223cc228
Matthew Johnson <mattmjohnson33@gmail.com>
2017-02-13 21:45:48 -0800

    added object splitting feature

--- source/ota.js
+++ source/ota.js
@@ -1,4 +1,15 @@
 class Ota {
+
+    getKeysAndValues(inputObject){
+        let keys = [];
+        let values = [];
+
+        for(let key in inputObject){
+            keys.push(key);
+            values.push(inputObject[key]);
+        }
+        return [keys,values];
+    }
 }
 
 export default Ota;
